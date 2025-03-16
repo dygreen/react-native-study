fea86c07606a8a678078f95f9666a8a5162c9bc3
dygreen <ehdus9877@naver.com>
2025-03-16 22:40:02 +0900

    #feat : List Views - Our Menu, Contact Us

--- app/(coffee)/_layout.tsx
+++
@@ -1,7 +0,0 @@
-import { Slot } from 'expo-router'
-
-import React from 'react'
-
-export default function CoffeeLayout() {
-    return <Slot />
-}
--- app/index.jsx
+++ app/index.jsx
@@ -19,8 +19,13 @@ const app = () => {
                 style={styles.image}
             >
                 <Text style={styles.title}>Coffee shop</Text>
+                <Link href="/menu" style={{ marginHorizontal: 'auto' }} asChild>
+                    <Pressable style={styles.button}>
+                        <Text style={styles.buttonText}>Our Menu</Text>
+                    </Pressable>
+                </Link>
                 <Link
-                    href="/(coffee)/contact"
+                    href="/contact"
                     style={{ marginHorizontal: 'auto' }}
                     asChild
                 >
@@ -66,10 +71,12 @@ const styles = StyleSheet.create({
     },
     button: {
         height: 60,
+        width: 150,
         borderRadius: 20,
         justifyContent: 'center',
         backgroundColor: 'rgba(0,0,0,0.75)',
         padding: 6,
+        marginBottom: 50,
     },
     buttonText: {
         color: 'white',
