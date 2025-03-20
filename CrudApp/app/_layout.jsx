bca85f1473bfaa7682366fac39134874d232a8fe
dygreen <ehdus9877@naver.com>
2025-03-20 22:33:01 +0900

    #feat : crud app

--- CrudApp/app/_layout.jsx
+++ CrudApp/app/_layout.jsx
@@ -1,6 +1,15 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 export default function RootLayout() {
-  return <SafeAreaProvider><Stack /></SafeAreaProvider>
+    return (
+        <SafeAreaProvider>
+            <Stack>
+                <Stack.Screen
+                    name="index"
+                    options={{ headerShown: false }}
+                ></Stack.Screen>
+            </Stack>
+        </SafeAreaProvider>
+    )
 }
